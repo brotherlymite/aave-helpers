53f7671c75b1e878abf8eada50f4ef6ee4f04161
Harsh Pandey <harshsatishpandey@gmail.com>
2023-03-02 12:14:40 +0530

    test: config engine

--- src/test/AaveV3ConfigEngineTest.t.sol
+++ src/test/AaveV3ConfigEngineTest.t.sol
@@ -3,25 +3,24 @@ pragma solidity ^0.8.0;
 
 import 'forge-std/Test.sol';
 
-import {AaveV3Polygon, AaveV3PolygonAssets} from 'aave-address-book/AaveV3Polygon.sol';
-import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
-import {AaveMisc} from 'aave-address-book/AaveMisc.sol';
 import {IAaveV3ConfigEngine} from '../v3-config-engine/IAaveV3ConfigEngine.sol';
-import {IV3RateStrategyFactory} from '../v3-config-engine/V3RateStrategyFactory.sol';
 import {AaveV3PolygonMockListing} from './mocks/AaveV3PolygonMockListing.sol';
-import {ITransparentProxyFactory} from 'solidity-utils/contracts/transparent-proxy/interfaces/ITransparentProxyFactory.sol';
-import {DeployRatesFactoryPolLib} from '../../script/V3RateStrategyFactory.s.sol';
-import {DeployEnginePolLib} from '../../script/AaveV3ConfigEngine.s.sol';
+import {AaveV3EthereumMockCapUpdate} from './mocks/AaveV3EthereumMockCapUpdate.sol';
+import {AaveV3AvalancheCollateralUpdate} from './mocks/AaveV3AvalancheCollateralUpdate.sol';
+import {AaveV3OptimismMockRatesUpdate} from './mocks/AaveV3OptimismMockRatesUpdate.sol';
+import {DeployRatesFactoryPolLib, DeployRatesFactoryEthLib, DeployRatesFactoryAvaLib, DeployRatesFactoryOptLib} from '../../script/V3RateStrategyFactory.s.sol';
+import {DeployEnginePolLib, DeployEngineEthLib, DeployEngineAvaLib, DeployEngineOptLib} from '../../script/AaveV3ConfigEngine.s.sol';
+import {AaveGovernanceV2, AaveV3Ethereum, AaveV3Polygon, AaveV3Optimism, AaveV3Avalanche, AaveMisc} from 'aave-address-book/AaveAddressBook.sol';
+import {AaveV3PolygonAssets} from 'aave-address-book/AaveV3Polygon.sol';
+import {AaveV3OptimismAssets} from 'aave-address-book/AaveV3Optimism.sol';
+import {IDefaultInterestRateStrategy} from 'aave-address-book/AaveV3.sol';
 import '../ProtocolV3TestBase.sol';
 
 contract AaveV3ConfigEngineTest is ProtocolV3TestBase {
   using stdStorage for StdStorage;
 
-  function setUp() public {
+  function testListing() public {
     vm.createSelectFork(vm.rpcUrl('polygon'), 39797440);
-  }
-
-  function testEngine() public {
     (address ratesFactory, ) = DeployRatesFactoryPolLib.deploy();
 
     IAaveV3ConfigEngine engine = IAaveV3ConfigEngine(DeployEnginePolLib.deploy(ratesFactory));
@@ -101,5 +100,135 @@ contract AaveV3ConfigEngineTest is ProtocolV3TestBase {
         variableDebtToken: engine.VTOKEN_IMPL()
       })
     );
+  }
+
+  function testCapsUpdates() public {
+    vm.createSelectFork(vm.rpcUrl('mainnet'), 16727659);
+    (address ratesFactory, ) = DeployRatesFactoryEthLib.deploy();
+
+    IAaveV3ConfigEngine engine = IAaveV3ConfigEngine(DeployEngineEthLib.deploy(ratesFactory));
+    AaveV3EthereumMockCapUpdate payload = new AaveV3EthereumMockCapUpdate(engine);
+
+    vm.startPrank(AaveV3Ethereum.ACL_ADMIN);
+    AaveV3Ethereum.ACL_MANAGER.addPoolAdmin(address(payload));
+    vm.stopPrank();
+
+    ReserveConfig[] memory allConfigsBefore = _getReservesConfigs(AaveV3Ethereum.POOL);
+    payload.execute();
+    ReserveConfig[] memory allConfigsAfter = _getReservesConfigs(AaveV3Ethereum.POOL);
+
+    ReserveConfig memory expectedAssetConfig = ReserveConfig({
+      symbol: allConfigsBefore[6].symbol,
+      underlying: allConfigsBefore[6].underlying,
+      aToken: allConfigsBefore[6].aToken,
+      variableDebtToken: allConfigsBefore[6].variableDebtToken,
+      stableDebtToken: allConfigsBefore[6].stableDebtToken,
+      decimals: allConfigsBefore[6].decimals,
+      ltv: allConfigsBefore[6].ltv,
+      liquidationThreshold: allConfigsBefore[6].liquidationThreshold,
+      liquidationBonus: allConfigsBefore[6].liquidationBonus,
+      liquidationProtocolFee: allConfigsBefore[6].liquidationProtocolFee,
+      reserveFactor: allConfigsBefore[6].reserveFactor,
+      usageAsCollateralEnabled: allConfigsBefore[6].usageAsCollateralEnabled,
+      borrowingEnabled: allConfigsBefore[6].borrowingEnabled,
+      interestRateStrategy: allConfigsBefore[6].interestRateStrategy,
+      stableBorrowRateEnabled: allConfigsBefore[6].stableBorrowRateEnabled,
+      isActive: allConfigsBefore[6].isActive,
+      isFrozen: allConfigsBefore[6].isFrozen,
+      isSiloed: allConfigsBefore[6].isSiloed,
+      isBorrowableInIsolation: allConfigsBefore[6].isBorrowableInIsolation,
+      isFlashloanable: allConfigsBefore[6].isFlashloanable,
+      supplyCap: 1_000_000,
+      borrowCap: allConfigsBefore[6].borrowCap,
+      debtCeiling: allConfigsBefore[6].debtCeiling,
+      eModeCategory: allConfigsBefore[6].eModeCategory
+    });
+
+    _validateReserveConfig(expectedAssetConfig, allConfigsAfter);
+  }
+
+  function testCollateralsUpdates() public {
+    vm.createSelectFork(vm.rpcUrl('avalanche'), 26871634);
+    (address ratesFactory, ) = DeployRatesFactoryAvaLib.deploy();
+
+    IAaveV3ConfigEngine engine = IAaveV3ConfigEngine(DeployEngineAvaLib.deploy(ratesFactory));
+    AaveV3AvalancheCollateralUpdate payload = new AaveV3AvalancheCollateralUpdate(engine);
+
+    vm.startPrank(AaveV3Avalanche.ACL_ADMIN);
+    AaveV3Avalanche.ACL_MANAGER.addPoolAdmin(address(payload));
+    vm.stopPrank();
+
+    ReserveConfig[] memory allConfigsBefore = _getReservesConfigs(AaveV3Avalanche.POOL);
+    payload.execute();
+    ReserveConfig[] memory allConfigsAfter = _getReservesConfigs(AaveV3Avalanche.POOL);
+
+    ReserveConfig memory expectedAssetConfig = ReserveConfig({
+      symbol: allConfigsBefore[6].symbol,
+      underlying: allConfigsBefore[6].underlying,
+      aToken: allConfigsBefore[6].aToken,
+      variableDebtToken: allConfigsBefore[6].variableDebtToken,
+      stableDebtToken: allConfigsBefore[6].stableDebtToken,
+      decimals: allConfigsBefore[6].decimals,
+      ltv: 62_00,
+      liquidationThreshold: 72_00,
+      liquidationBonus: 106_00, // 100_00 + 6_00
+      liquidationProtocolFee: allConfigsBefore[6].liquidationProtocolFee,
+      reserveFactor: allConfigsBefore[6].reserveFactor,
+      usageAsCollateralEnabled: allConfigsBefore[6].usageAsCollateralEnabled,
+      borrowingEnabled: allConfigsBefore[6].borrowingEnabled,
+      interestRateStrategy: allConfigsBefore[6].interestRateStrategy,
+      stableBorrowRateEnabled: allConfigsBefore[6].stableBorrowRateEnabled,
+      isActive: allConfigsBefore[6].isActive,
+      isFrozen: allConfigsBefore[6].isFrozen,
+      isSiloed: allConfigsBefore[6].isSiloed,
+      isBorrowableInIsolation: allConfigsBefore[6].isBorrowableInIsolation,
+      isFlashloanable: allConfigsBefore[6].isFlashloanable,
+      supplyCap: allConfigsBefore[6].supplyCap,
+      borrowCap: allConfigsBefore[6].borrowCap,
+      debtCeiling: allConfigsBefore[6].debtCeiling,
+      eModeCategory: allConfigsBefore[6].eModeCategory
+    });
+
+    _validateReserveConfig(expectedAssetConfig, allConfigsAfter);
+  }
+
+  function testRateStrategiesUpdates() public {
+    vm.createSelectFork(vm.rpcUrl('optimism'), 74562421);
+    (address ratesFactory, ) = DeployRatesFactoryOptLib.deploy();
+
+    IAaveV3ConfigEngine engine = IAaveV3ConfigEngine(DeployEngineOptLib.deploy(ratesFactory));
+    AaveV3OptimismMockRatesUpdate payload = new AaveV3OptimismMockRatesUpdate(engine);
+
+    vm.startPrank(AaveV3Optimism.ACL_ADMIN);
+    AaveV3Optimism.ACL_MANAGER.addPoolAdmin(address(payload));
+    vm.stopPrank();
+
+    IDefaultInterestRateStrategy initialStrategy = IDefaultInterestRateStrategy(
+      AaveV3OptimismAssets.USDT_INTEREST_RATE_STRATEGY
+    );
+    payload.execute();
+    address updatedStrategyAddress = AaveV3Optimism.AAVE_PROTOCOL_DATA_PROVIDER.getInterestRateStrategyAddress(AaveV3OptimismAssets.USDT_UNDERLYING);
+
+    InterestStrategyValues memory expectedInterestStrategyValues = InterestStrategyValues({
+      addressesProvider: address(AaveV3Optimism.POOL_ADDRESSES_PROVIDER),
+      optimalUsageRatio: _bpsToRay(80_00),
+      baseVariableBorrowRate: initialStrategy.getBaseVariableBorrowRate(),
+      variableRateSlope1: initialStrategy.getVariableRateSlope1(),
+      variableRateSlope2: _bpsToRay(75_00),
+      stableRateSlope1: initialStrategy.getStableRateSlope1(),
+      stableRateSlope2: _bpsToRay(75_00),
+      baseStableBorrowRate: initialStrategy.getBaseStableBorrowRate(),
+      optimalStableToTotalDebtRatio: initialStrategy.OPTIMAL_STABLE_TO_TOTAL_DEBT_RATIO()
+    });
+
+    _validateInterestRateStrategy(
+      updatedStrategyAddress,
+      updatedStrategyAddress,
+      expectedInterestStrategyValues
+    );
+  }
+
+  function _bpsToRay(uint256 amount) internal pure returns (uint256) {
+    return (amount * 1e27) / 10_000;
   }
 }
